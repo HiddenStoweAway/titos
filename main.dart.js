d7f425c278c176164d4a971db34fa97eae9710b3
HiddenStoweAway <HiddenStoweAway@users.noreply.github.com>
2026-03-19 04:08:45 +0000

    deploy: 0fe0a7dfdf1a1ea4a1bdde4b29f6de980c593694

--- main.dart.js
+++ main.dart.js
@@ -26717,7 +26717,7 @@ $S:2}
 A.ar8.prototype={
 $1(a){var s=A.db().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/e4b8dca3f1b4ede4c30371002441c88c12187ed6/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/052f31d115eceda8cbff1b3481fcde4330c4ae12/":s)+a},
 $S:132}
 A.LH.prototype={
 gA(a){var s=this.a
